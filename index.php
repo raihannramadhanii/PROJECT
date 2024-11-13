250a12e18630dd58b38c303e862bd7f970e26f69
ihwanariga07 <ihwanariga.ia@gmail.com>
2024-11-13 14:50:00 +0700

    membuat file index

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+    include_once('navbar.php');
+    ?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+        <div class="card">
+  <div class="card-header text-center">
+    <h4> Sistem Informasi Mahasiswa </h4>
+  </div>
+  <div class="card-body">
+  <form action="proses_pert3.php" method="POST" enctype="multipart/form-data">
+  <div class="mb-3">
+  <div class="card-body">
+  <form action="proses_pert3.php" method="POST" enctype="multipart/form-data">
+  <div class="mb-3">
+    
+  </div>
+
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="reset" class="btn btn-primary">Reset</button>
+</form>
+  </div>
+</div>
+</div>
+
+
+   <script src="js/bootstrap.js"></script> 
+   <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
+</head>
+<body>
+    
+</body>
+</html>
